44dc02458cb4405c828b863f204e7c4380a1420a
Rizky Lanang Sadana Mulyono Putra <rizkylsmp@gmail.com>
2025-11-15 14:53:09 +0700

    Overlay change pw

--- frontend/src/Components/Tools/ChangePassword.jsx
+++ frontend/src/Components/Tools/ChangePassword.jsx
@@ -47,7 +47,7 @@ const ChangePassword = ({
   return (
     <Dialog.Root open={open} onOpenChange={handleClose}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
+        <Dialog.Overlay className="fixed inset-0 bg-dark/50 data-[state=open]:animate-overlayShow" />
         <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
           <Dialog.Title className="m-0 text-[17px] font-medium text-mauve12">
             {title}
